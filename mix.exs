5896fc724d6a89b2ea017bc1129d221cdc20485b
Thiago Oliveira Marçal <thiagoomarcal@protonmail.com>
2022-05-10 20:52:04 -0300

    Implementing routing with Elixir and GraphQL Pt2

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Wabanex.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:absinthe, "~> 1.5"},
-      {:absinthe_plug, "~> 1.5"}
+      {:absinthe_plug, "~> 1.5"},
+      {:crudry, "~> 2.4"}
     ]
   end
 
